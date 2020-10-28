f2bd733226a80594589d0e94b15a73a94fb87f7b
William Pannell <lpann5@gmail.com>
2020-10-28 22:28:46 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,5 @@
 # Write your #display_rainbow method here
-def display_rainbow()
+def display_rainbowdisplay_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+puts ""
 
 end
